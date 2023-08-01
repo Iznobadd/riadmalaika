6883273b61500e4e1de71c4eeecbd3b5329bdb8d
Iznobad <brandon.cebi.pro@gmail.com>
2023-08-01 12:29:16 +0200

    marge footer + add lightbox in menu card

--- src/components/table/Menu.jsx
+++ src/components/table/Menu.jsx
@@ -4,32 +4,51 @@ import Entree from "../../assets/images/carte_entree.jpg";
 import Plat from "../../assets/images/carte_plat.jpg";
 import Dessert from "../../assets/images/carte_dessert.jpg";
 import { Flip } from "react-awesome-reveal";
+import Lightbox from "../partials/Lightbox";
+
+
 function Menu() {
+
   return (
     <div className="carte">
       <div className="container">
-        <div className="all_cartes">
-          <div>
-            <Flip direction="vertical" duration={1500}>
-              <img src={Signature} alt="Carte Signature Riad Malaïka" />
-            </Flip>
-          </div>
-          <div>
-            <Flip direction="vertical" duration={1500} delay={800}>
-              <img src={Entree} alt="Carte des entrées Riad Malaïka" />
-            </Flip>
-          </div>
-          <div>
-            <Flip direction="vertical" duration={1500} delay={1600}>
-              <img src={Plat} alt="Carte des plats Riad Malaïka" />
-            </Flip>
-          </div>
-          <div>
-            <Flip direction="vertical" duration={1500} delay={2400}>
-              <img src={Dessert} alt="Carte des desserts Riad Malaïka" />
-            </Flip>
-          </div>
-        </div>
+        <Lightbox
+          galleryID="menu-gallery"
+          images={[
+            {
+              largeURL:
+                Signature,
+              thumbnailURL:
+                Signature,
+              width: 1080,
+              height: 1920,
+            },
+            {
+              largeURL:
+                Entree,
+              thumbnailURL:
+              Entree,
+              width: 1080,
+              height: 1920,
+            },
+            {
+              largeURL:
+                Plat,
+              thumbnailURL:
+                Plat,
+              width: 1080,
+              height: 1920,
+            },
+            {
+              largeURL:
+                Dessert,
+              thumbnailURL:
+                Dessert,
+              width: 1080,
+              height: 1920,
+            },
+          ]}
+        />
       </div>
     </div>
   );
