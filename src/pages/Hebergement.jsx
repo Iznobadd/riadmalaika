d9f36d4811b00bfcff68926371905a8453d1aaaa
Iznobad <brandon.cebi.pro@gmail.com>
2025-02-19 17:50:18 +0100

    change menu

--- src/pages/Hebergement.jsx
+++ src/pages/Hebergement.jsx
@@ -12,6 +12,28 @@ function Hebergement() {
       <div className="chambres-banner">
         <h2>Hébergement</h2>
       </div>
+      <div className="container">
+        <div className="intro-chambres text-center">
+          <h2>Liste de nos petites attentions en chambre</h2>
+          <p>Bouteille de champagne</p>
+          <div className="separator-chambres"></div>
+          <p>Assortiments de Patisserie orientales</p>
+          <div className="separator-chambres"></div>
+          <p>Corbeille de fruits</p>
+          <div className="separator-chambres"></div>
+          <p>Pétales de roses sur le lit</p>
+          <div className="separator-chambres"></div>
+          <p>Flacon Huile massante à l’huile d’argan</p>
+          <div className="separator-chambres"></div>
+          <p>Bouquet de 20 roses</p>
+          <div className="separator-chambres"></div>
+          <p>
+            Formule romance : Bouteille de champagne, Bouquet de 20 roses et
+            Pétales de roses
+          </p>
+        </div>
+      </div>
+
       <ListChambres />
       {/* <div className="reservation-chambres" id="reservation">
         <div className="container">
